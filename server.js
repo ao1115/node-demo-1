0d744cdf52b769ab0ccb6a54f4c6fbc0eff85892
ao1115 <82885390+ao1115@users.noreply.github.com>
2021-05-19 22:43:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ var server = http.createServer(function (request, response) {
     </html>
 `);
     response.end();
-  } else if (path === "/x") {
+  } else if (path === "/style.css") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
     response.write(`h1{color: red;}`);
